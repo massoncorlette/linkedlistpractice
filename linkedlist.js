0ffb85e4cf491a5326a6b1ad53f2c61784d3ed1a
massoncorlette <massoncorlette07@gmail.com>
2024-09-27 00:37:07 -0400

    using temp variables to be able to reference objects that are in memory

--- linkedlist.js
+++ linkedlist.js
@@ -14,26 +14,65 @@ let linkedList = function() {
 
   let head = new nodeObj();
 
+  let appendValue = function(value) {
+    let newObj = new nodeObj(value);
+    let endOfList = getTail();
+    endOfList.nextNode = newObj;
+  }
+
   let prependValue = function(value) {
     let newObj = new nodeObj(value);
     let beginningOfList = getHead();
-    newObj.nextNode = beginningOfList.value;
+    newObj.nextNode = beginningOfList;
     head = newObj;
   }
 
   let size = function() {
+    let currentValue = head;
+    let size = 0;
+
+    while (!(currentValue == null)) {
+      currentValue = currentValue.nextNode;
+      size += 1;
+    }
+    console.log(size);
   }
 
   let getHead = function() {
     return head;
   }
 
-  let indexLocation = function() {
+  let getTail = function() {
+    let currentValue = head;
 
+    while (!(currentValue.nextNode == null)) {
+      currentValue = currentValue.nextNode;
+    }
+    return currentValue;
+  }
+
+  let indexLocation = function(index) {
+    let currentValue = head.value;
+    let currentNode = head;
+    let i = 0;
+
+    while (i < index) {
+      currentNode = currentNode.nextNode;
+      currentValue = currentNode.value;
+      i++;
+    }
+    console.log(currentValue);
   }
 
   let pop = function() {
+    let currentHead = getHead();
+    let tempNode = null;
 
+    while (!(currentHead.nextNode == null)) {
+      tempNode = currentHead;
+      currentHead = currentHead.nextNode;
+    }
+    tempNode.nextNode = null;
   }
 
   let contains = function(value) {
@@ -45,18 +84,20 @@ let linkedList = function() {
   }
 
   let toString = function() {
-    let currentValue = head.value;
+    let currentValue = head;
 
     while (!(currentValue == null)) {
-      console.log(currentValue + " -> ");
+      console.log(currentValue.value + " -> ");
       currentValue = currentValue.nextNode;
     }
 
   }
   return {
+    appendValue:appendValue,
     prependValue:prependValue,
     size:size,
-    getHead:getHead,
+    getHead:getHead, 
+    getTail:getTail,
     indexLocation:indexLocation,
     pop:pop,
     contains:contains,
@@ -68,7 +109,15 @@ let linkedList = function() {
 const list = new linkedList();
 
 list.prependValue("dog");
+list.prependValue("cat");
+list.prependValue("fish");
+list.appendValue("bird");
+list.appendValue("spiders");
+list.size();
+list.indexLocation(4);
+list.pop();
 
 
-console.log(list.toString());
+
+list.toString();
 
